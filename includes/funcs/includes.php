25c646803bbbd77a49dab412dd0da959c58c6e94
spantal <spantaleo@moog.com>
2016-03-07 12:51:58 -0500

    Update some styles

--- includes/funcs/includes.php
+++ includes/funcs/includes.php
@@ -8,14 +8,9 @@ function generateHead($breadcrum){
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Frank 'n Stein and Son,LLC</title>
-    <!-- Bootstrap -->
+    <title>Frank 'N Stein and Son,LLC</title>
     <link href='includes/css/bootstrap.min.css' rel='stylesheet'>
     <link rel='stylesheet' href='includes/css/ffs.css'/>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>
       <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
@@ -93,7 +88,7 @@ echo "</div><!--Main Body  -->
 				·
 				<a href='contact.php'>Contact</a>
 			</p>
-			<p class='footer-company-name'>Frank n' Stein and Son, LLC &copy; 2015</p>
+			<p class='footer-company-name'>Frank 'N Stein and Son, LLC &copy; 2016</p>
 		</footer>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 	<script src='includes/js/jquery-1.12.1.min.js'></script>
